fe68e503a599c26ca6c8b3eb6712f278a763481f
Adam Kurowski <adam.kurowski@stepstone.com>
2020-06-18 08:16:04 +0200

    CDK First Lambda

--- cdk/iam-manager/app.py
+++ cdk/iam-manager/app.py
@@ -4,8 +4,21 @@ from aws_cdk import core
 
 from iam_manager.iam_manager_stack import IamManagerStack
 
+import configparser
+
+
+# Read Config File
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+app_envs = {
+    'env':core.Environment(
+        account=config.get('MAIN','account_id'),
+        region=config.get('MAIN','region')
+    )
+}
 
 app = core.App()
-IamManagerStack(app, "iam-manager")
+IamManagerStack(app, "iam-manager",**app_envs)
 
 app.synth()
--- cdk/iam-manager/iam_manager/iam_manager_stack.py
+++ cdk/iam-manager/iam_manager/iam_manager_stack.py
@@ -1,9 +1,19 @@
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_apigateway as gw,
+    aws_lambda as lambda_
+)
 
 
 class IamManagerStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        role_changer = lambda_.Function(self,"RoleChanger",
+            runtime = lambda_.Runtime.PYTHON_3_8,
+            code =  lambda_.Code.from_asset("lambdas/switcher"),
+            handler = "main.handler",
+        )
 
         # The code that defines your stack goes here
