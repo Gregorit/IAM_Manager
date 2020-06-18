643ba800c256f6bfb6c3accace86e5001190d1da
Adam Kurowski <adam.kurowski@stepstone.com>
2020-06-18 19:59:20 +0200

    Add Table

--- cdk/iam-manager/iam_manager/iam_manager_stack.py
+++ cdk/iam-manager/iam_manager/iam_manager_stack.py
@@ -3,7 +3,8 @@ from aws_cdk import (
     aws_apigateway as agw,
     aws_lambda as lambda_,
     aws_cloudtrail as cloudtrail,
-    aws_glue as glue
+    aws_glue as glue,
+    aws_athena as athena
 )
 
 
@@ -29,8 +30,67 @@ class IamManagerStack(core.Stack):
 
         # CloudTrail 
         tail = cloudtrail.Trail(self,'CloudTrail')
+        db_name = 'cloudtrail'
+        db = glue.Database(self,'cloudtrail',database_name=db_name)
 
-        db = glue.Database(self,'cloudtrail',database_name='cloudtrail')
+        query = """
+        CREATE EXTERNAL TABLE cloudtrail_logs (
+eventversion STRING,
+useridentity STRUCT<
+               type:STRING,
+               principalid:STRING,
+               arn:STRING,
+               accountid:STRING,
+               invokedby:STRING,
+               accesskeyid:STRING,
+               userName:STRING,
+sessioncontext:STRUCT<
+attributes:STRUCT<
+               mfaauthenticated:STRING,
+               creationdate:STRING>,
+sessionissuer:STRUCT<  
+               type:STRING,
+               principalId:STRING,
+               arn:STRING, 
+               accountId:STRING,
+               userName:STRING>>>,
+eventtime STRING,
+eventsource STRING,
+eventname STRING,
+awsregion STRING,
+sourceipaddress STRING,
+useragent STRING,
+errorcode STRING,
+errormessage STRING,
+requestparameters STRING,
+responseelements STRING,
+additionaleventdata STRING,
+requestid STRING,
+eventid STRING,
+resources ARRAY<STRUCT<
+               ARN:STRING,
+               accountId:STRING,
+               type:STRING>>,
+eventtype STRING,
+apiversion STRING,
+readonly STRING,
+recipientaccountid STRING,
+serviceeventdetails STRING,
+sharedeventid STRING,
+vpcendpointid STRING
+)
+ROW FORMAT SERDE 'com.amazon.emr.hive.serde.CloudTrailSerde'
+STORED AS INPUTFORMAT 'com.amazon.emr.cloudtrail.CloudTrailInputFormat'
+OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://CloudTrail_bucket_name/AWSLogs/Account_ID/CloudTrail/';  
+"""
+
+        table = athena.CfnNamedQuery(self,'cloudtrail_table',
+            database = db_name,
+            query_string = query
+        )
+
+    
         
 
 
