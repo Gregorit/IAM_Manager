c49aef93392d7b675cc129561dd41e15486c0e2a
Adam Kurowski <adam.kurowski@stepstone.com>
2020-06-18 08:41:05 +0200

    .

--- cdk/iam-manager/lambdas/switcher/main.py
+++ cdk/iam-manager/lambdas/switcher/main.py
@@ -4,5 +4,5 @@ def handler(event, context):
             'headers': {
                 'Content-Type': 'text/plain'
             },
-            'body': 'Incredible aint it.'
+            'body': 'Make it work !.'
     }
