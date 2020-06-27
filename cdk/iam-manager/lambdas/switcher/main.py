739a496dfe7a58c4429118b1ae380b89f6d3166f
Gregorit <grzegorz.urych@gmail.com>
2020-06-27 22:11:59 +0200

    Move switcher script location

--- cdk/iam-manager/lambdas/switcher/main.py
+++ cdk/iam-manager/lambdas/switcher/main.py
@@ -1,8 +1,58 @@
-def handler(event, context): 
-    return {
+# Internal Modules
+import os
+
+# External Modules
+import boto3  # available as default in AWS Lambda
+
+
+learning_mode = os.environ['LEARNING_MODE']
+learning_group = os.environ['LEARNING_GROUP']
+username = os.environ['USERNAME']
+
+
+def handler(event, context):    
+    # Start session and select IAM client
+    session = boto3.session.Session()
+    iam = session.client('iam')
+    success = False
+
+    # Mode switcher
+    if learning_mode:
+        try:
+            iam.add_user_to_group(
+                GroupName=learning_group,
+                UserName=username
+            )
+            success = True
+            print(f"User {username} has been added to {learning_group} group.")
+        except:
+            print(f"User {username} is already added to {learning_group} group.")
+    else:
+        try:
+            iam.remove_user_from_group(
+                GroupName=learning_group,
+                UserName=username
+            )
+            success = True
+            print(f"User {username} has been removed from {learning_group} group.")
+        except:
+            print(f"User {username} is not in {learning_group} group.")
+
+    
+    
+    if success:
+        return {
             'statusCode': 200,
             'headers': {
                 'Content-Type': 'text/plain'
             },
-            'body': 'Switcher !.'
-    }
+            'body': 'Lambda finished with code 0.'
+        }
+    else:
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'text/plain'
+            },
+            'body': 'Lambda finished with code 1.'
+        }
