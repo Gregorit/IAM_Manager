8eeea2c2f97760e88ef3dc3e41c1aedde59874d6
Gregorit <grzegorz.urych@gmail.com>
2020-07-02 23:21:12 +0200

    Add separation for each service

--- cp_parser/learner.py
+++ cp_parser/learner.py
@@ -15,7 +15,7 @@ params = {
     'bucket' : 'iam-manager-cloudtrails310cd22f2-faslq2t36wt4',
     'path'  : 'athena_out',
     'query': f'SELECT * FROM "cloudtrail"."trail_logs" WHERE "useridentity"."arn"  = \'arn:aws:iam::789552300344:user/{username}\''
-}
+    }
 session = boto3.session.Session()
 location, data= athena_from_s3.query_results(session, params)
 print("Locations", location)
@@ -43,19 +43,30 @@ print(f"Single: {rights[1]}\n\n")
 rights_list = [key for key, value in rights.items() if value > 0 and "eu-west-1" in key]
 pp(rights_list)
 
-with open('policy_template.json', 'r+') as f:
-    data = json.load(f)
+with open('generated_policy.json', 'w') as f:
+    data = {"Statement": []}
+
+    services_list = []
     for right in rights_list:
         right = right.split(':')
 
+        if right[2] not in services_list:
+            services_list.append(right[2])
+            data['Statement'].append({
+                "Effect": "Allow",
+                "Action": [],
+                "Resource": []
+                })
+
+
         action = f"{right[2]}:{right[5]}"
-        dest = data['Statement'][0]['Action']
+        dest = data['Statement'][services_list.index(right[2])]['Action']
         if action not in dest:
             dest.append(action)
         
         resource = f"{right[0]}:{right[1]}:{right[2]}:{right[3]}:{right[4]}:*"
-        dest = data['Statement'][0]['Resource']
+        dest = data['Statement'][services_list.index(right[2])]['Resource']
         if resource not in dest:
             dest.append(resource)
-    
-    pp(data)
+
+    json.dump(data, f, indent=4)
