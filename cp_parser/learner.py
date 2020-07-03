384b687db43fcde56c58ef94fae8076d84f53651
Adam Kurowski <adam.kurowski@stepstone.com>
2020-07-03 10:30:24 +0200

    .

--- cp_parser/learner.py
+++ cp_parser/learner.py
@@ -8,13 +8,16 @@ import json
 from collections import defaultdict
 from pprint import pprint as pp
 
-username = 'akurow'
+
+
+role_user_group_arn = "arn:aws:iam::789552300344:user/akurow"
+
 params = {
     'region' : 'eu-west-1',
     'database' : 'cloudtrail',
     'bucket' : 'iam-manager-cloudtrails310cd22f2-faslq2t36wt4',
     'path'  : 'athena_out',
-    'query': f'SELECT * FROM "cloudtrail"."trail_logs" WHERE "useridentity"."arn"  = \'arn:aws:iam::789552300344:user/{username}\''
+    'query': f'SELECT * FROM "cloudtrail"."trail_logs" WHERE "useridentity"."arn"  = \'{role_user_group_arn}\''
     }
 session = boto3.session.Session()
 location, data= athena_from_s3.query_results(session, params)
