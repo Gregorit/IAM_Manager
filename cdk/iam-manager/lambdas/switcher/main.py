8d5410ca2a30b04c382e9db1f8b3aedea2e0ef71
Gregorit <grzegorz.urych@gmail.com>
2020-07-04 00:34:29 +0200

    Edit switcher Lambda for role compatibility

--- cdk/iam-manager/lambdas/switcher/main.py
+++ cdk/iam-manager/lambdas/switcher/main.py
@@ -5,19 +5,22 @@ import os
 import boto3  # available as default in AWS Lambda
 
 
-learning_mode = os.environ['LEARNING_MODE']
-learning_group = os.environ['LEARNING_GROUP']
-username = os.environ['USERNAME']
+role_user_group_arn = event['queryStringParameters']['arn']
+learning_group = 'administrator'
+learning_policy = 'AdministratorAccess'
 
 
-def handler(event, context):    
+def handler(event, context):
+    arn_fragments = role_user_group_arn.split(':')
+
     # Start session and select IAM client
     session = boto3.session.Session()
     iam = session.client('iam')
     success = True
 
     # Mode switcher
-    if learning_mode:
+    if arn_fragments[5] == "user":
+        username = arn_fragments[5].strip('/')[1]
         try:
             iam.add_user_to_group(
                 GroupName=learning_group,
@@ -26,34 +29,42 @@ def handler(event, context):
             print(f"User {username} has been added to {learning_group} group.")
         except:
             print(f"User {username} is already added to {learning_group} group.")
-    else:
+    
+    elif arn_fragments[5] == "role":
+        rolename = arn_fragments[5].strip('/')[1]
         try:
-            iam.remove_user_from_group(
-                GroupName=learning_group,
-                UserName=username
-            )
-            print(f"User {username} has been removed from {learning_group} group.")
-        except:
-            print(f"User {username} is not in {learning_group} group.")
-            success = False
-
-
-    if learning_mode and success:
-        try:
-            print(f"Launching CodeBuild...")
-            codebuild = session.client('codebuild')
-            codebuild.start_build(
-                projectName = "mainpipelineprojectA75A748F-Q84zgcuZfXVF",
-                artifactsOverride={
-                    'type': 'CODEPIPELINE'
-                }
+            iam.attach_role_policy(
+                RoleName=rolename,
+                PolicyArn=learning_policy
             )
         except:
-            print("CodeBuild launching failed!")
-            success = False
+            print(f"Policy {learning_policy} has been added to {rolename} role.")
+    # else:
+    #     try:
+    #         iam.remove_user_from_group(
+    #             GroupName=learning_group,
+    #             UserName=username
+    #         )
+    #         print(f"User {username} has been removed from {learning_group} group.")
+    #     except:
+    #         print(f"User {username} is not in {learning_group} group.")
+    #         success = False
 
-    
-    
+    # CodeBuild launch
+    try:
+        print(f"Launching CodeBuild...")
+        codebuild = session.client('codebuild')
+        codebuild.start_build(
+            projectName = "mainpipelineprojectA75A748F-Q84zgcuZfXVF",
+            artifactsOverride={
+                'type': 'CODEPIPELINE'
+            }
+        )
+    except:
+        print("CodeBuild launching failed!")
+        success = False
+
+
     if success:
         return {
             'statusCode': 200,
