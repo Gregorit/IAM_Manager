cc9f963110d16891e222d343e6025510907ea92b
Gregorit <grzegorz.urych@gmail.com>
2020-07-07 12:00:28 +0200

    ...

--- cp_parser/main.py
+++ cp_parser/main.py
@@ -91,10 +91,10 @@ def handler():
         if action not in dest:
             dest.append(action)
         
-        # resource = f"{right[0]}:{right[1]}:{right[2]}:::*"
+        resource = "*"
         dest = generated_policy['Statement'][services_list.index(right[2])]['Resource']
         if resource not in dest:
-            dest.append("*")
+            dest.append(resource)
 
 
     # Check if policy exists, remove it if yes
